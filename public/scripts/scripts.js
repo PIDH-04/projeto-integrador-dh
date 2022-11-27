ecd59062280ac94051673eab4cd7b953d109006d
Julio Cesar Bueno <buenojc@outlook.com>
2022-11-27 19:24:44 -0300

    add: arquivo de scripts e funcionalidade no seletor de quantidade

---
+++ public/scripts/scripts.js
@@ -0,0 +1,15 @@
+// Botões para acrescer e decrescer quantidade do produto
+const btnSubtrai = document.getElementById('btn-subtrai');
+const btnAdiciona = document.getElementById('btn-adiciona');
+const quantidade = document.getElementById('quantidade');
+
+
+btnSubtrai.addEventListener('click',  e => {
+    if(quantidade.value > 1){
+        quantidade.value = parseInt(quantidade.value) - 1;
+    }
+})
+
+btnAdiciona.addEventListener('click', e => {
+    quantidade.value = parseInt(quantidade.value) + 1;
+})
