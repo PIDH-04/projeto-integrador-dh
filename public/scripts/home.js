607d007ddd9b6b53728dfb2d345146298299c474
KillianDB <killiandomingues57@gmail.com>
2023-01-07 18:11:27 -0300

    fix: igualar figuras vitrine

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,18 +1,31 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     let slides = document.querySelectorAll('.slides');
-let home = 0;
+    let home = 0;
 
-function next(){
-    slides[home].classList.remove('active');
-    home = (home + 1) % slides.length;
-    slides[home].classList.add('active');
-}
 
-function prev(){
-    slides[home].classList.remove('active');
-    home = (home - 1 + slides.length) % slides.length;
-    slides[home].classList.add('active');
-}
+    function next() {
+        slides[home].classList.remove('active');
+        home = (home + 1) % slides.length;
+        slides[home].classList.add('active');
 
-setInterval (next, 3000);
+    }
+
+    function prev() {
+        slides[home].classList.remove('active');
+        home = (home - 1 + slides.length) % slides.length;
+        slides[home].classList.add('active');
+
+    }
+
+    setInterval(next, 3000);
+})
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+prev.addEventListener('click', () => {
+    home--
+})
+next.addEventListener('click', () => {
+    home++
 })
