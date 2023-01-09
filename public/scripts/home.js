fd2564bb75ee94d47470d2169ae325c2a00da806
KillianDB <killiandomingues57@gmail.com>
2023-01-09 09:42:50 -0300

    fix: botão banner funcional

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,18 +1,31 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     let slides = document.querySelectorAll('.slides');
-let home = 0;
+    let home = 0;
+    let prev = document.getElementById('prev');
+    let next = document.getElementById('next')
 
-function next(){
-    slides[home].classList.remove('active');
-    home = (home + 1) % slides.length;
-    slides[home].classList.add('active');
-}
+    function nextAtual() {
+        slides[home].classList.remove('active');
+        home = (home + 1) % slides.length;
+        slides[home].classList.add('active');
+    }
 
-function prev(){
-    slides[home].classList.remove('active');
-    home = (home - 1 + slides.length) % slides.length;
-    slides[home].classList.add('active');
-}
+    function prevAtual() {
+        slides[home].classList.remove('active');
+        home = (home - 1 + slides.length) % slides.length;
+        slides[home].classList.add('active');
+    }
 
-setInterval (next, 3000);
+    setInterval(nextAtual, 3000);
+
+    prev.addEventListener('click', () => {
+        prevAtual()
+    })
+
+    next.addEventListener('click', () => {
+        nextAtual()
+    })
 })
+
+
+
