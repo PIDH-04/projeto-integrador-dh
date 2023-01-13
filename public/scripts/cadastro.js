e1f601fd1b18eb61dbc9e0856602431a11d9608d
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2023-01-12 22:28:03 -0300

    arrumando validações dos inputs

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -1,66 +1,96 @@
-window.addEventListener('load', () => {
-    const nomeInput = document.getElementById('#nome')
-    const adressIntpu = document.getElementById('#adress')
-    const apartmentInput = document.getElementById('#apartment')
-    const cityInput = document.getElementById('#city')
-    const cityOpcional = document.getElementById('#opcional')
-    const paisCadastro = document.getElementById('#validationtooltip03')
-    const estadoCadastro = document.getElementById('#validationtooltip05')
+// window.addEventListener('load', () => {
+//     const nomeInput = document.getElementById('#nome')
+//     const adressIntpu = document.getElementById('#adress')
+//     const apartmentInput = document.getElementById('#apartment')
+//     const cityInput = document.getElementById('#city')
+//     const cityOpcional = document.getElementById('#opcional')
+//     const paisCadastro = document.getElementById('#validationtooltip03')
+//     const estadoCadastro = document.getElementById('#validationtooltip05')
+
+//     const errosCadastro = document.getElementById('errors')
+
+//     const erros = []
+
+
+//     nome.addEventListener('submit', function () {
+//         if (nomeInput.value.length < 1) {
+//             erros.push('nome input vazio')
+//         }
+
+//     })
+
+//     adress.addEventListener('submit', function () {
+//         if (adressInput.value.length < 1) {
+//             erros.push('adress input vazio')
+//         }
+
+//     })
+
+//     apartmentInput.addEventListener('submit', function () {
+//         if (apartmentInput.value.length < 1) {
+//             erros.push('apartament input vazio')
+//         }
+
+//     })
+
+//     cityInput.addEventListener('submit', function () {
+//         if (cityInput.value.length < 1) {
+//             erros.push('city input vazio')
+//         }
+
+//     })
+
+//     cityOpcional.addEventListener('submit', function () {
+//         if (cityOpcionalInput.value.length < 1) {
+//             erros.push('cityOpcioanl input vazio')
+//         }
+
+//     })
+
+//     paisCadastro.addEventListener('submit', function () {
+//         if (paisCadastroInput.value.length < 1) {
+//             erros.push('paisCadastro input vazio')
+//         }
+
+//     })
+
+
+
+
+//     btnEnviar.addEventListener("click", (e) => {
+//         quantidade.value = parseInt(quantidade.value) + 1;
+//       });
+
+
+
+// })
+
+document.querySelector('form').addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    const campos = [
+        document.querySelector('#nome'),
+        document.querySelector('#adress'),
+        document.querySelector('#apartant'),
+        document.querySelector('#city'),
+        document.querySelector('#opcional'),
+        document.querySelector('##validationtooltip03'),
+        document.querySelector('##validationtooltip05'),
+        document.querySelector('#city')
+    ];
 
     const errosCadastro = document.getElementById('errors')
 
-    const erros = []
+      const erros = []
 
+      campos.addEventListener('submit', function () {
+          if (campos.value.length < 1) {
+               erros.push('nome input vazio')
+            }
+        
+          })
 
-    nome.addEventListener('submit', function () {
-        if (nomeInput.value.length < 1) {
-            erros.push('nome input vazio')
-        }
-
-    })
-
-    adress.addEventListener('submit', function () {
-        if (adressInput.value.length < 1) {
-            erros.push('adress input vazio')
-        }
-
-    })
-
-    apartmentInput.addEventListener('submit', function () {
-        if (apartmentInput.value.length < 1) {
-            erros.push('apartament input vazio')
-        }
-
-    })
-
-    cityInput.addEventListener('submit', function () {
-        if (cityInput.value.length < 1) {
-            erros.push('city input vazio')
-        }
-
-    })
-
-    cityOpcional.addEventListener('submit', function () {
-        if (cityOpcionalInput.value.length < 1) {
-            erros.push('cityOpcioanl input vazio')
-        }
-
-    })
-
-    paisCadastro.addEventListener('submit', function () {
-        if (paisCadastroInput.value.length < 1) {
-            erros.push('paisCadastro input vazio')
-        }
-
-    })
-
-
-
-
-    btnEnviar.addEventListener("click", (e) => {
-        quantidade.value = parseInt(quantidade.value) + 1;
-      });
-
-
+        this.reset(); 
 
 })
