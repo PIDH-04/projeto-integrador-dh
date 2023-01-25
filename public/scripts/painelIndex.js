535a4fcfabfc25526efaa40ef90f7e778da1fe9f
helio Cunha <heliocunha.souza@gmail.com>
2023-01-24 21:44:21 -0300

    fix pagina plus status de pedido

--- public/scripts/painelIndex.js
+++ public/scripts/painelIndex.js
@@ -11,37 +11,22 @@ formulario.addEventListener('submit', function (event) {
    let erros = []
    let nameInput = document.getElementById('nome')
    if (nameInput.value == "") {
-      erros.push('o campo nome não podem estar vazio');
+      alert('o campo nome não podem estar vazio');
    }
    if (nameInput.value.length < 3) {
-      erros.push('o campo name tem que ser maior que 3')
+      alert('o campo nome tem que ser maior que 3')
    }
    let emailInput = document.getElementById('email')
    if (emailInput.value.length < 3) {
-      erros.push('o campo e-mail tem que ser maior que 3');
+      alert('o campo e-mail tem que ser maior que 3');
    }
    let senhaInput = document.getElementById('password')
    if (senhaInput.value.length < 3) {
-      erros.push('o campo senha tem que ser maior que 3');
+      alert('o campo senha tem que ser maior que 3');
    }
+   else if (password2.value != senhaInput.value) {
+   alert('senhas não conferem, digite novamente')
+}
    console.log(erros)
 
 })
-
-    //console.log("agora vai")
-    //<script>
-    // const form = document.getElementById('form');
-    // const campos = document.querySelectorAll('.required')
-    // const spans = document.querySelectorAll('.span-required')
-    // const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-
-    // function nameValidate() {
-    //   if (campos[0].value.length < 3){
-    //     console.log('O nome deve ter no minimo 3 caracteres')
-    //   }
-    //   else
-    //   {
-    //     console.log('validando')
-    //   }
-    // }
-  //</script>
