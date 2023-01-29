930b31ba5e0f3fa6d1003f8f8daf1a1bcb84e58f
wesleyaraujoal <wesleyarajo46391@gmail.com>
2023-01-29 02:26:04 -0300

    fix: adicionando caixa de erro

--- public/scripts/pagamento.js
+++ public/scripts/pagamento.js
@@ -2,30 +2,55 @@ window.addEventListener('load', function () {
 
     let reservationform = document.querySelector('form.reservada')
 
-    reservationform.addEventListener("submit", function (event) {
+    reservationform.addEventListener('submit', function (event) {
         event.preventDefault();
+       
+        let errorsMessagens = [];
+
 
         let fieldName = document.getElementById("nome")
 
-        if (fieldName.value == "") {
-            alert('o campo nome nao pode estar vazio')
+        if (fieldName.value == '') {
+            errorsMessagens.push('o campo nome nao pode estar vazio')
         } else if (fieldName.value.length < 3) {
-            alert("mome precisa ser maior")
+            errorsMessagens.push("Nome precisa ser maior")
         }
 
-        let fieldNumero = document.getElementById("Numero")
-        if (fieldNumero.value == "") {
-            alert('campo cartao esta vazio')
+        let fieldNumero = document.getElementById('Numero')
+        if (fieldNumero.value == '') {
+            errorsMessagens.push('Campo cartao esta vazio')
 
         }
 
-        let fieldMes = document.getElementById("mes")
-        console.log(fieldMes)
-        if (fieldMes.value == "") {
-            alert('campo mes  esta vazio')
+        let fieldMes = document.getElementById('mes')
+        if (fieldMes.value == '') {
+            errorsMessagens.push('Campo mes  esta vazio')
 
+        }
+
+        let fieldAno = document.getElementById('ano')
+        if (fieldAno.value == '') {
+            errorsMessagens.push('Campo ano esta vazio')
+        }
+
+        let fieldCvv = document.getElementById("cvv")
+        if (fieldCvv.value == '') {
+            errorsMessagens.push('Campo cvv esta vazio ')
+        }
+
+        if (errorsMessagens > 0) {
+           
+            event.preventDefault();
+
+            let divErrors = document.getElementById('div-errors');
+            divErrors.classList.remove('no-errors');
+            divErrors.classList.add('errors');
+
+            let ulErrors = document.querySelector('div.errors ul');
+            for (let i = 0; i < errorsMessagens.length; i++) {
+                ulErrors.innerHTML += '<li>' + errorsMessagens[i] + '</li>'
+            }
         }
     })
 })
-
 
