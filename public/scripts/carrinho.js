16e03b9a0fa1564acd364803073c84283bcb239f
Julio Cesar Bueno <buenojc@outlook.com>
2023-02-09 20:16:26 -0300

    add: valor dinâmico do subtotal e total

--- public/scripts/carrinho.js
+++ public/scripts/carrinho.js
@@ -1,36 +1,62 @@
 window.addEventListener("load", () => {
   const produtosNoCarrinho = JSON.parse(localStorage.getItem("produtos"));
   const produtosContainer = document.querySelector(".produtos");
-  console.log(produtosNoCarrinho);
+  const subtotalContainer = document.querySelector(".subtotal p");
+  const totalContainer = document.querySelector(".total p");
+  const valorProdutos = [];
+
+  function addProdutoNaPagina(produto) {
+    let valorFormatado = produto.preco.replace("R$", "");
+    valorFormatado = valorFormatado.replace(",", ".");
+    valorProdutos.push(valorFormatado);
+
+    const produtoContainer = document.createElement("div");
+    produtoContainer.classList.add("produto");
+    produtoContainer.innerHTML = `
+              <div class="img-container">
+                <a href="/produto">
+                  <img
+                      src="${produto.img}"
+                      alt="${produto.nome}"
+                  />
+                </a>
+              </div>
+              <div class="produto-info">
+                <a href="/produto">
+                  <h2>${produto.nome}</h2>
+                </a>
+                <p class="variacao">Tampo madeira</p>
+                <p class="quantidade">Quantidade: ${produto.quantidade}</p>
+                <p class="preco">${produto.preco}</p>
+              </div>
+              <span class="removeBtn">Remover</span>
+          `;
+    produtosContainer.appendChild(produtoContainer);
+  }
+
+  function addInfosDePreco(arrayComvalores) {
+    const soma = arrayComvalores.reduce(
+      (anterior, atual) => parseFloat(anterior) + parseFloat(atual)
+    );
+    const somaFormatada = soma.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+    subtotalContainer.innerText = somaFormatada;
+    totalContainer.innerText = somaFormatada;
+  }
 
   if (produtosNoCarrinho === null) {
     produtosContainer.innerHTML = `<p>Você ainda não adicionou nenhum produto no seu carrinho </p>`;
   } else {
     produtosContainer.innerHTML = ``;
-
     for (let produto of produtosNoCarrinho) {
-      const produtoContainer = document.createElement("div");
-      produtoContainer.classList.add("produto");
-      produtoContainer.innerHTML = `
-                <div class="img-container">
-                  <a href="/produto">
-                    <img
-                        src="${produto.img}"
-                        alt="${produto.nome}"
-                    />
-                  </a>
-                </div>
-                <div class="produto-info">
-                  <a href="/produto">
-                    <h2>${produto.nome}</h2>
-                  </a>
-                  <p class="variacao">Tampo madeira</p>
-                  <p class="quantidade">Quantidade: ${produto.quantidade}</p>
-                  <p class="preco">${produto.preco}</p>
-                </div>
-                <span>Remover</span>
-            `;
-      produtosContainer.appendChild(produtoContainer);
+      console.log(produto.nome);
+      addProdutoNaPagina(produto);
     }
+    addInfosDePreco(valorProdutos);
+
+    const removerBtn = document.querySelectorAll(".removeBtn");
+
   }
 });
