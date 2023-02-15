81ab585eab2bf6e9b05e9747fbe2f2fff192dbd0
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2023-02-15 15:02:30 -0300

    add: criar cadastro

---
+++ public/services/ProdurosServices.js
@@ -0,0 +1,48 @@
+const Produtos =require('../databasesCategorias.json');
+
+
+function listarProdutos(){
+    return Produtos;
+
+}
+
+function getprodutById(id) {
+    const Produtos = Produtos.find(c => c.id === id);
+    if (Produtos) {
+      return Produtos;
+    } else {
+      return null;
+    }
+
+    
+  }
+
+  
+  function criarProduto(produto) {
+
+    // Gerar um ID único para o produto
+  
+    const ultimoID = produto.length > 0 ? Math.max(produto.map(p => p.id)) : 0;
+    const id = ultimoID + 1;
+  
+    // Adicionar o ID ao objeto de produto
+    produto.id = id;
+  
+    // Adicionar o produto ao objeto JSON
+    produtos.push(produto);
+  
+    // Retornar o produto criado
+    return produto;
+  }
+
+
+  function categoria (){
+    const produtosCategorias = produtos.filter(p=> p.categoria === categoria);
+
+    return produtosCategorias;
+
+  }
+
+  
+
+module.exports = Produtos;
