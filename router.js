4be9b21c2099afaeff596a272c1b3bfa80c9c168
KillianDB <killiandomingues57@gmail.com>
2023-03-06 16:17:41 -0300

    fix: alterando rota url de listagem de produtos

--- router.js
+++ router.js
@@ -24,17 +24,18 @@ router.get('/login', UsuarioController.login);
 
 router.get('/login/email', UsuarioController.loginEmail);
 
-router.get('/checkoutpagamento', UsuarioController.checkoutPagamento);
+// router.get('/checkoutpagamento', UsuarioController.checkoutPagamento);
 
 router.get("/checkoutDeEndereco", UsuarioController.checkoutEndereco);
 
 router.get("/produto/:idDoProduto", ProdutosController.show);
 
-router.get("/produtos/:link?", ProdutosController.listagem);
+router.get("/categorias/:id?", ProdutosController.listagem);
+// o '?' torna o id dispensavel, podendo acessar o url '/categorias'
 
 router.get("/cadastro", UsuarioController.showCadastro);
-router.get("/painelUsuario", UsuarioController.showPainelUsuario);
-router.get("/statusDePedidos", UsuarioController.showstatusDePedido);
+// router.get("/painelUsuario", UsuarioController.showPainelUsuario);
+// router.get("/statusDePedidos", UsuarioController.showstatusDePedido);
 
 
 // Admin Routers
