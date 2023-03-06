30a990a10711b42ec285bff1f58f99e036bc56f8
KillianDB <killiandomingues57@gmail.com>
2023-03-06 16:20:04 -0300

    fix: alterando os controllers geral e de produtos para usar as funções do services

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -1,19 +1,34 @@
-const categorias = require('../databases/Categorias.json');
-const produtos = require('../databases/Produtos.json');
+const CategoriasServices = require('../services/CategoriasServices');
+const ProdutosServices = require('../services/ProdutosServices');
 
 const ProdutosControllers = {
   show: (req, res) => {
+     // Mostrar categorias para header
+     const categorias = CategoriasServices.listarCategorias();
+
     let id = req.params.idDoProduto;
     const produto = produtos.find( p => p.id == id);
 
     return res.render('produto', {categorias, produto});
   },
   listagem: (req, res) => {
-    let link = req.params.link;
-    const categoria = categorias.find( c => c.link == link);
+     // Mostrar categorias para header
+     const categorias = CategoriasServices.listarCategorias();
+
+    //Listar produtos
+    const produtos = ProdutosServices.listarProdutos();
+
+    //Pegar parametro da url e alterar a categoria mostrada
+    let id = req.params.id;
+    const categoria = CategoriasServices.mostrarCategoria(id);
+
+    //Retorna view
     return res.render('listagemProdutos', {categorias, produtos, categoria});
   },
   showCarrinho: (req, res) => {
+     // Mostrar categorias para header
+     const categorias = CategoriasServices.listarCategorias();
+     
     return res.render('carrinho', {categorias, produtos});
   },
 };
