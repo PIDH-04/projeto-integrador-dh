4b9edce436633f9356e694fe5c4fe9199c96debd
KillianDB <killiandomingues57@gmail.com>
2023-03-06 16:27:54 -0300

    fix: deletando função categoriaId sobressalente do ProdutosServices

---
+++ services/ProdutosServices.js
@@ -0,0 +1,94 @@
+const produtos = require('../databases/Produtos.json');
+const fs = require('fs');
+
+
+function criarProduto(produto) {
+
+  // Encontra o último ID dos produtos existentes e adiciona 1 para gerar um novo ID
+  const ultimoID = produtos[produtos.length -1].id
+  const id = ultimoID + 1;
+
+
+  // Adiciona o ID ao objeto de produto
+  produto.id = id;
+
+  // Adiciona o produto ao array de produtos
+  produtos.push(produto);
+
+  // Escreve o array atualizado de produtos no arquivo JSON
+  fs.writeFileSync('./databases/Produtos.json', JSON.stringify(produtos,null,4));
+
+  // Retorna o produto criado
+  return produto;
+}
+
+function editarProduto(id, novoProduto) {
+
+  // Encontrar o índice do produto a ser editado pelo ID
+  const index = produtos.findIndex(p => p.id === id);
+  if (index !== -1) {
+
+    // Atualizar o produto com os dados do novo produto
+    const produtoAtualizado = {
+      ...produtos[index],
+      ...novoProduto
+    };
+
+    // Substituir o produto antigo pelo produto atualizado no array de produtos
+    produtos[index] = produtoAtualizado;
+
+    // Escrever os dados atualizados no arquivo JSON
+    fs.writeFileSync('./databases/Produtos.json', JSON.stringify(produtos,null,4));
+
+    // Retornar o produto atualizado
+    return produtoAtualizado;
+  } else {
+
+    // Retornar null se não encontrar o produto a ser editado
+    return null;
+  }
+}
+
+function listarProdutos() {
+  return produtos;
+
+}
+
+function mostrarProdutoId(id) {
+  const produto = produtos.find(c => c.id === id);
+  return produto || null;
+}
+
+function excluirProdutoId(id) {
+  // Encontrar o índice do produto a ser excluído pelo ID
+  const indiceProduto = produtos.findIndex(p => p.id === id);
+
+  if (indiceProduto !== -1) {
+      // Remover o produto do array de produtos
+      produtos.splice(indiceProduto, 1);
+
+      // Escrever os dados atualizados no arquivo JSON
+      fs.writeFileSync('./databases/Produtos.json', JSON.stringify(produtos,null,4));
+
+      // Retornar true se o produto foi excluído com sucesso
+      return true;
+  } else {
+      // Retornar false se não encontrar o produto a ser excluído
+      return false;
+  }
+}
+
+function listarProdutosCategoria(categoria) {
+  const produtosFiltrados = produtos.filter(produto => produto.categoria === categoria);
+  return produtosFiltrados.length > 0 ? produtosFiltrados : null;
+}
+
+module.exports = {
+  criarProduto,
+  editarProduto,
+  listarProdutos,
+  mostrarProdutoId,
+  excluirProdutoId,
+  listarProdutosCategoria,
+  
+}
