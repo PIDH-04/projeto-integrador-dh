d9f9b0036e07d4959014b1a8a70e202089131ef8
wesleyaraujoal <wesleyarajo46391@gmail.com>
2023-03-08 11:52:13 -0300

    Deletando funcao

--- services/UsuariosServices.js
+++ services/UsuariosServices.js
@@ -22,28 +22,6 @@ function criarUsuario(usuario) {
     return usuario;
   }
 
-
-  function excluirProdutoId(id) {
-    // Encontrar o índice do produto a ser excluído pelo ID
-    const indiceProduto = produtosSite.findIndex(p => p.id === id);
-  
-    if (indiceProduto !== -1) {
-        // Remover o produto do array de produtos
-        produtosSite.splice(indiceProduto, 1);
-  
-        // Escrever os dados atualizados no arquivo JSON
-        fs.writeFileSync('./databases/Produtos.json', JSON.stringify(produtosSite,null,4));
-  
-        // Retornar true se o produto foi excluído com sucesso
-        return true;
-    } else {
-        // Retornar false se não encontrar o produto a ser excluído
-        return false;
-    }
-  }
-
-
-
   
 module.exports = {
 
