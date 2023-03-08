40f5c43b694b50b3097e0b1a9947d8601e3c6638
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2023-03-08 00:04:27 -0300

    ok

---
+++ services/UsuariosServices.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const UsuariosSite = require('../databases/Usuarios.json');
+
+
+function excluirUsuarios(id) {
+    // Encontrar o índice do produto a ser excluído pelo ID
+    const indiceUsuario = UsuariosSite.findIndex(p => p.id === id);
+  
+    if (indiceProduto !== -1) {
+        // Remover o produto do array de produtos
+        UsuariosSite.splice(indiceUsuario, 1);
+  
+        // Escrever os dados atualizados no arquivo JSON
+        fs.writeFileSync('./databases/Usuarios.json', JSON.stringify(UsuariosSite,null,4));
+  
+        // Retornar true se o produto foi excluído com sucesso
+        return true;
+    } else {
+        // Retornar false se não encontrar o produto a ser excluído
+        return false;
+    }
+  }
