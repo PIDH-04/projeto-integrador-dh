6af7e4b19186b725a8a7451e391f33953b91077e
Julio Cesar Bueno <buenojc@outlook.com>
2023-03-08 21:42:51 -0300

    add: preview da img no upload

--- public/scripts/adminProduto.js
+++ public/scripts/adminProduto.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
+    const campoPreview = document.getElementById('preview-img')
+    const inputImg = document.getElementById('img')
+    
+    function mostraImagemPreview(e){
+        if(e.target.files.length > 0){
+            let src = URL.createObjectURL(event.target.files[0])
+            campoPreview.src = src;
+        }
+    }
+
+    inputImg.addEventListener('change', mostraImagemPreview)
 
 })
