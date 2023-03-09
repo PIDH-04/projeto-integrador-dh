9a56ad02d2da923b82976a1a3b5dbb4cc15e58cf
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2023-03-09 00:27:41 -0300

    add: criando função de excluir

--- tests/ExcluirUsuario.test.js
+++ tests/ExcluirUsuario.test.js
@@ -1,28 +1,4 @@
-const fs = require('fs');
-const UsuariosSite = require('../databases/Usuarios.json');
+const UsuariosServices = require('../services/UsuariosServices');
 
 
-function excluirUsuarios(id) {
-    // Encontrar o índice do produto a ser excluído pelo ID
-    const indiceUsuario = UsuariosSite.findIndex(p => p.id === id);
-  
-    if (indiceProduto !== -1) {
-        // Remover o produto do array de produtos
-        UsuariosSite.splice(indiceUsuario, 1);
-  
-        // Escrever os dados atualizados no arquivo JSON
-        fs.writeFileSync('./databases/Usuarios.json', JSON.stringify(UsuariosSite,null,4));
-  
-        // Retornar true se o produto foi excluído com sucesso
-        return true;
-    } else {
-        // Retornar false se não encontrar o produto a ser excluído
-        return false;
-    }
-  }
-
-  excluirUsuarios(3);
-
-
-
-
+console.log(UsuariosServices.excluirUsuarioId(3));
