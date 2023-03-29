fa9ad2d2527550c5f220aecc22f95c6c58946be7
Julio Cesar Bueno <buenojc@outlook.com>
2023-03-29 13:12:43 -0300

    add: checagem da rota de criação de produto

--- helpers/validacoesExpress.js
+++ helpers/validacoesExpress.js
@@ -5,6 +5,15 @@ const validacoesExpress = {
     check("email").notEmpty().bail().isEmail(),
     check("senha").notEmpty().bail().isLength({ min: 3 }),
   ],
+  criacaoProduto: [
+    check("nome").notEmpty().bail().trim(),
+    check("largura").notEmpty().bail().isNumeric().trim(),
+    check("altura").notEmpty().bail().isNumeric().trim(),
+    check("comprimento").notEmpty().bail().isNumeric().trim(),
+    check("preco").notEmpty().bail().isNumeric().trim(),
+    check("categoria").notEmpty(),
+    check("area").notEmpty()
+  ]
 };
 
 module.exports = validacoesExpress;
