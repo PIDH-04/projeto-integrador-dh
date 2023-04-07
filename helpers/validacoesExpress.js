53c0876cfc7f6a3889404031d33fa52117cfe0d2
Julio Cesar Bueno <buenojc@outlook.com>
2023-04-06 22:09:25 -0300

    fix: Mensagens de erro na criação do produto

--- helpers/validacoesExpress.js
+++ helpers/validacoesExpress.js
@@ -6,14 +6,25 @@ const validacoesExpress = {
     check("senha").notEmpty().bail().isLength({ min: 3 }),
   ],
   criacaoProduto: [
-    check("nome").notEmpty().bail().trim(),
-    check("largura").notEmpty().bail().isNumeric().trim(),
-    check("altura").notEmpty().bail().isNumeric().trim(),
-    check("comprimento").notEmpty().bail().isNumeric().trim(),
-    check("preco").notEmpty().bail().isNumeric().trim(),
-    check("categoria").notEmpty(),
-    check("area").notEmpty()
-  ]
+    check("nome").notEmpty().bail().trim().withMessage('O nome do produto deve ser preenchido'),
+    check("descricao").notEmpty().bail().trim().withMessage('A descrição deve ser preenchida'),
+    check("largura").notEmpty().withMessage('A largura deve ser informada')
+      .bail().isNumeric().withMessage('largura inválida'),
+    check("altura").notEmpty().withMessage('A altura deve ser preenchida')
+      .bail().isNumeric().trim().withMessage('altura inválida'),
+    check("comprimento").notEmpty().withMessage('O comprimento deve ser preenchido')
+      .bail().isNumeric().trim().withMessage('comprimento inválido'),
+    check("preco").notEmpty().withMessage('O preço deve ser preenchido')
+      .bail().isNumeric().trim().withMessage('preço inválido'),
+    check("categoria").notEmpty().withMessage('A categoria deve ser selecionada'),
+    check("area").notEmpty().withMessage('A área deve ser preenchida'),
+    check("imagens").custom((value, { req }) => {
+      if (req.files.length < 1) {
+        return false;
+      }
+      return true;
+    }).withMessage('É necessário adicionar uma foto ao produto')
+  ],
 };
 
 module.exports = validacoesExpress;
