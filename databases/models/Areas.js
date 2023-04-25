d9f8ffa9eb6ad5671f9cc5256b64f897fb86044e
KillianDB <killiandomingues57@gmail.com>
2023-04-25 12:29:49 -0300

    model areas funcionando(testada)

---
+++ databases/models/Areas.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Areas = sequelize.define(
+        'Areas',
+        {
+            id:{
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            nome:{
+                type: DataTypes.STRING(45),
+                allowNull: false
+            }
+        },
+        {
+            tableName: "areas",
+            timestamps: true
+        }
+    )
+        
+    Areas.associate = (models) => {
+        Areas.hasMany(
+            models.Produtos,
+            {as: "produtos", foreignKey: "produtos_id"}
+        );
+    }
+
+    return Areas;
+
+}
