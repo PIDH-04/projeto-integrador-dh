5a758e4def7971261131567cd612f2e5fb63dc4a
KillianDB <killiandomingues57@gmail.com>
2023-05-08 18:49:26 -0300

    tornando as funções GeralController assincronas

--- controllers/GeralController.js
+++ controllers/GeralController.js
@@ -1,18 +1,12 @@
 const CategoriasServices = require('../services/CategoriasServices');
 const ProdutosServices = require('../services/ProdutosServices');
-const {Produtos} = require('../databases/models');
 
 const GeralController = {
-    home: async (req, res) => {
-      // Mostrar categorias para header e footer
-      const categorias = await CategoriasServices.listarCategorias();
-        return res.render('home', {categorias});
-      }
-
-      
-
-
-
+  home: async (req, res) => {
+    // Mostrar categorias para header e footer
+    const categorias = await CategoriasServices.listarCategorias();
+    return res.render('home', { categorias });
+  }
 };
 
 module.exports = GeralController;
