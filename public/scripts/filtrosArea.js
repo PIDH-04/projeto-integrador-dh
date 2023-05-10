062031ef5f635b888d3fd5089873e6eeac818771
KillianDB <killiandomingues57@gmail.com>
2023-05-10 12:15:35 -0300

    filtragem do produto alterando quando clica no outro checkbox

--- public/scripts/filtrosArea.js
+++ public/scripts/filtrosArea.js
@@ -1,18 +1,18 @@
-window.addEventListener('load', function () {
-    let areaexterna = document.querySelector('#areaexterna');
-    let areainterna = document.querySelector('#areainterna');
+let areaexterna = document.querySelector('#areaexterna');
+let areainterna = document.querySelector('#areainterna');
+let idCategoria = document.querySelector('#idCategoria');
+console.log(idCategoria.dataset.idcategoria)
 
-    areaexterna.addEventListener('change', function () {
-        if (this.checked) {
-            let url = window.location.href + "/2";
-            window.location.href = url;
-        }
-    });
+areaexterna.addEventListener('change', function(){
+  if(this.checked){
+    let url = window.location.origin + "/categorias/" + idCategoria.dataset.idcategoria + "/2";
+    window.location.href = url;
+  }
+});
 
-    areainterna.addEventListener('change', function () {
-        if (this.checked) {
-            let url = window.location.href + "/1"
-            window.location.href = url;
-        }
-    });
-})
+areainterna.addEventListener('change', function(){
+    if(this.checked){
+        let url = window.location.origin + "/categorias/" + idCategoria.dataset.idcategoria + "/1"
+        window.location.href = url;
+    }
+});
