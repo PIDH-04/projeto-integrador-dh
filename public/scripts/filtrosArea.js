1a56a726eebb34c4a8bddac59e5ef564d9c15435
KillianDB <killiandomingues57@gmail.com>
2023-05-12 11:58:36 -0300

    ajuste de filtro de area da listagem de produtos para manter o checkbox checked

--- public/scripts/filtrosArea.js
+++ public/scripts/filtrosArea.js
@@ -1,24 +1,44 @@
-let areaexterna = document.querySelector('#areaexterna');
-let areainterna = document.querySelector('#areainterna');
-let idCategoria = document.querySelector('#idCategoria');
-let limpafiltro = document.querySelector('#limpafiltro');
-/* console.log(idCategoria.dataset.idcategoria) */
+window.addEventListener('load', function () {
+  let areaexterna = document.querySelector('#areaexterna');
+  let areainterna = document.querySelector('#areainterna');
+  let idCategoria = document.querySelector('#idCategoria');
+  let limpafiltro = document.querySelector('#limpafiltro');
+  /* console.log(idCategoria.dataset.idcategoria) */
+  let urlBase = window.location.origin + "/categorias/" + idCategoria.dataset.idcategoria;
 
-areaexterna.addEventListener('change', function(){
-  if(this.checked){
-    let url = window.location.origin + "/categorias/" + idCategoria.dataset.idcategoria + "/2";
-    window.location.href = url;
-  }
-});
-
-areainterna.addEventListener('change', function(){
-    if(this.checked){
-        let url = window.location.origin + "/categorias/" + idCategoria.dataset.idcategoria + "/1"
-        window.location.href = url;
+  function checandoPagina() {
+    if(window.location.href !== urlBase){
+      let path = window.location.pathname;
+      let arrayPath = path.split('/')
+      if(arrayPath[arrayPath.length-1] == '2'){
+        areaexterna.checked = true
+      }
+      else if(arrayPath[arrayPath.length-1] == '1'){
+        areainterna.checked = true
+      }
     }
-});
+  }
+  checandoPagina();
+  areaexterna.addEventListener('change', function () {
+    if (this.checked = true) {
+      let url = urlBase + "/2";
+      window.location.href = url;
+    }else{
+      window.location.href = urlBase;
+    }
+  });
 
-limpafiltro.addEventListener('click', function(){
-    let url = window.location.origin + "/categorias/" + idCategoria.dataset.idcategoria
-    window.location.href = url;
+  areainterna.addEventListener('change', function () {
+    if (this.checked = true) {
+      let url = urlBase + "/1"
+      window.location.href = url;
+    }else{
+      window.location.href = urlBase;
+    }
+  });
+
+  limpafiltro.addEventListener('click', function () {
+    window.location.href = urlBase;
+  })
 })
+
